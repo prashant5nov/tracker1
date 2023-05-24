ba554e74c33f8bdea4c1b435e389c4bfee7213a8
prashant5nov <prashantvelocity123@gmail.com>
2023-05-24 22:44:49 +0530

    updates

--- api/handlers/film_v1.py
+++ api/handlers/film_v1.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+from api.responses.detail import DetailResponse
+from api.repository.film.mongo import MongoFilmRepository
 
 
 router = APIRouter(prefix="/api/v1/films", tags=["films"])
 
 
+# @router.post("/", status_code=201, response_model=DetailResponse):
+# async def post_create_film(
+#         film: Body(..., title="Film", description="the film details"),
+#         repo: MongoFilmRepository = Depends(film_repository),
+# )
